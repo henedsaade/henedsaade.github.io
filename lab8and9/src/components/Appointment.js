0c1c795836af0c51c67f16c3e504c14a35a56cb4
Hened Saade <hsaad097@uottawa.ca>
2021-07-18 18:13:59 -0400

    Update Appointment.js

--- lab8and9/src/components/Appointment.js
+++ lab8and9/src/components/Appointment.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Appointment = () => {
     return ( 
         <div id="appointment" class=" p-3 my-3 border bg-gradient" style={{'background':'rgb(138, 215, 227)'}}>
-            <h1 style={{'color':'rgba(0, 0, 153, 1', 'font-weight':'bold'}}>Prenez rendez-vous avec nous pour choisir votre animal à adopter!</h1>
+            <h1 style={{'color':'rgba(0, 0, 153, 1', 'font-weight':'bold'}}>Prenez un rendez-vous avec nous pour choisir votre animal à adopter!</h1>
             <div class="row justify-content-around">
             </div>
             <Information />
